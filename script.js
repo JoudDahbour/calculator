86f3bf5f7ad96c44e26cfcbf0dce02e825b1a551
JoudDahbour <Jouddahbour1@gmail.com>
2026-07-07 00:18:47 -0400

    Updated Script.js

--- script.js
+++ script.js
@@ -16,3 +16,112 @@ const deleteButton = document.querySelector(".key-delete");
 const decimalButton = document.querySelector(".key-decimal");
 const undoButton = document.querySelector(".key-undo");
 
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    if (b === 0) {
+        return "Error: you can't divide by zero";
+    }
+    return a / b;
+}
+
+function operate(operator, a, b){
+    if (operator === "+") {
+        return add(a, b);
+    }
+    if (operator === "-") {
+        return subtract(a, b);
+    }
+    if (operator === "*") {
+        return multiply(a, b);
+    }
+    if (operator === "/") {
+        return divide(a, b);
+    }
+}
+
+function roundDisplay(number){
+    return Math.round(number * 10000) / 10000;
+}
+
+function operatorSymbol(operator){
+    if (operator === "+") {
+        return "+";
+    }
+    if (operator === "-") {
+        return "-";
+    }
+    if (operator === "*") {
+        return "×";
+    }
+    if (operator === "/") {
+        return "÷";
+    }
+    return "";
+}
+
+function saveState(){
+    historyStack.push({
+        firstOperand: firstOperand,
+        secondOperand: secondOperand,
+        operator: previousOperand,
+        display: display,
+        waiting: waiting,
+        previousOperation: previousOperand
+    });
+}
+
+function undo(){
+    if (historyStack.length === 0) {
+        return;
+    }
+    const lastState = historyStack.pop();
+    firstOperand = lastState.firstOperand;
+    secondOperand = lastState.secondOperand;
+    previousOperand = lastState.previousOperation;
+    display = lastState.display;
+    waiting = lastState.waiting;
+}
+
+function inputDigit(digit){
+    if (waiting){
+        display = digit;
+        waiting = false;
+    } else if (display === "0") {
+        display = digit;
+    } else {
+        display = display + digit;
+    }
+}
+
+function inputDecimal (){
+    if (waiting) {
+        display = "0.";
+        waiting = false;
+        return;
+    }
+    if (!display.includes(".")) {
+        display += ".";
+    }
+}
+
+function handleOperator(nextOperator){
+    if (isNan(parseFloat(display))) {
+        return;
+    }
+    const inputValue = parseFloat(display);
+
+    if (firstOperand !== null && waiting){
+        previousOperand = nextOperator;
+        return;
+    }
